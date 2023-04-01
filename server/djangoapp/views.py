c1f452101bfe243e467c90c5adc8b9f958f02eca
felipe0rojas <frbandera@gmail.com>
2023-04-01 02:08:12 -0300

    Login and logout functions added

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -27,17 +27,37 @@ def contact(request):
     return render(request,'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    # Handles POST request
+    if request.method == "POST":
+
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+
+        else:
+            return render(request, 'djangoapp/index.html', context)
+
+    else:
+        return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    print("Log out the user `{}`".format(request.user.username))
+    logout(request)
+    return redirect('djangoapp:index')
+
 
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
     return render(request,'djangoapp/registration.html')
-# ...
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
@@ -50,10 +70,10 @@ def get_dealerships(request):
 
 def get_dealer_details(request, dealer_id):
     return HttpResponse(dealer_id)
-# ...
+
 
 # Create a `add_review` view to submit a review
 def add_review(request, dealer_id):
     pass
-# ...
+
 
